1507e02a5b1aaeee328a068bdd06d994f93c4824
duynn912 <39177680+duynn912@users.noreply.github.com>
2023-09-11 15:50:50 +0700

    Update JPAConfig.java
    put your password here

--- src/main/java/com/laptrinhjavaweb/config/JPAConfig.java
+++ src/main/java/com/laptrinhjavaweb/config/JPAConfig.java
@@ -51,7 +51,7 @@ public class JPAConfig {
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/springmvcbasicfree");
 		dataSource.setUsername("root");
-		dataSource.setPassword("Duy09121995@");
+		dataSource.setPassword(".....");//put your password here
 		return dataSource;
 	}
 	
